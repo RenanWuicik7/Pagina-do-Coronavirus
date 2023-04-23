16a93e3a9d9263d5d14aca90b226ae9e717bce5a
Renan Cezar Wuicik <renan.wuicik.15@gmail.com>
2023-04-23 20:04:11 -0300

    Adicionando pasta com arquivo inutilizado

---
+++ type-js/aboutCorona.js
@@ -0,0 +1,16 @@
+/*
+function firstToggle() {
+  const content = document.querySelector(".i-0-cont");
+  content.classList.toggle('clicked');
+}
+
+function secToggle() {
+  const content = document.querySelector(".i-1-cont");
+  content.classList.toggle('clicked');
+}
+
+function thirdToggle() {
+  const content = document.querySelector(".i-2-cont");
+  content.classList.toggle('clicked');
+}
+*/
